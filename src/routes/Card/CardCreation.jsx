52e83c4054271ced60bdf81f38d095131783646d
gon2410x <gonzalo.virtual.2020@gmail.com>
2022-10-14 19:39:29 -0300

    update selection of form options

--- src/routes/Card/CardCreation.jsx
+++ src/routes/Card/CardCreation.jsx
@@ -1,13 +1,16 @@
-import { useContext} from 'react';
+import { useContext, useEffect, useState} from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { CardsContext } from '../../context/CardsContext';
 import { getCards } from '../../service';
 import {RiMapPinLine} from "react-icons/ri";
 import "./CardCreation.css"
+import { CONTINENTE, PAIS_O_CIUDAD, PROVINCIAS } from './list_of_database_time_zone';
 
 const CardCreation = () => {
   const { cards, setCards } = useContext( CardsContext )
+  const [ region, setRegion ] = useState(0)
+  const [ provincia, setProvincia] = useState(0)
   const navigate = useNavigate()
 
   const {
@@ -29,13 +32,9 @@ const CardCreation = () => {
         temperatura : data1.current_weather.temperature,
         velocidadViento : data1.current_weather.windspeed
       }
-      console.log('aqui es el new : ' , newCard)
-      console.log('one',cards)
-      setCards([...cards,newCard])
-      console.log('two',cards)
-      navigate('/')
 
-      
+      setCards([...cards,newCard])
+      navigate('/')      
     } )
     .catch((err) => console.log(err));
   }
@@ -55,12 +54,8 @@ const CardCreation = () => {
             ...register('continente',{
             required:'debe ingresar Latitud',})
           }>
-          <option>America</option>
-          <option>Africa</option>
-          <option>Asia</option>
-          <option>Europe</option>
-          <option >Oceanic</option>          
-          
+
+          {CONTINENTE.map((continente) => <option onClick={ () => {setRegion(CONTINENTE.indexOf(continente)); if(continente !== "America") setProvincia(0)}}>{continente}</option> )}
         </select>
         <br/>       
           
@@ -71,139 +66,20 @@ const CardCreation = () => {
             required:'debe ingresar pais',})
           }>
             
-            <optgroup label="America">
-            <option>	Anguila	</option>
-          <option>	Old and bearded	</option>
-          <option>	Argentina	</option>
-          <option>	Aruba	</option>
-          <option>	Bahamas	</option>
-          <option>	Barbados	</option>
-          <option>	Belize	</option>
-          <option>	Bermuda	</option>
-          <option>	Bolivia	</option>
-          <option>	Bonaire	</option>
-          <option>	Brazil	</option>
-          <option>	Canada	</option>
-          <option>	Chili	</option>
-          <option>	Colombia	</option>
-          <option>	Costa Rica	</option>
-          <option>	Cuba	</option>
-          <option>	Curaçao	</option>
-          <option>	Dominica	</option>
-          <option>	Ecuador	</option>
-          <option>	The Savior	</option>
-          <option>	USA	</option>
-          <option>	Grenade	</option>
-          <option>	Greenland	</option>
-          <option>	Guadeloupe	</option>
-          <option>	Guatemala	</option>
-          <option>	French Guiana	</option>
-          <option>	Guyanese	</option>
-          <option>	Haiti	</option>
-          <option>	Honduras	</option>
-          <option>	Clipperton Island	</option>
-          <option>	Navassa Island	</option>
-          <option>	Cayman Islands	</option>
-          <option>	Falkland Islands	</option>
-          <option>	Turks and Caicos Islands	</option>
-          <option>	British Virgin Islands	</option>
-          <option>	United States Virgin Islands	</option>
-          <option>	Jamaica	</option>
-          <option>	Martinique	</option>
-          <option>	Mexico	</option>
-          <option>	Montserrat	</option>
-          <option>	Nicaragua	</option>
-          <option>	Panama	</option>
-          <option>	Paraguay	</option>
-          <option>	Peru	</option>
-          <option>	Puerto Rico	</option>
-          <option>	Dominican Republic	</option>
-          <option>	Saba	</option>
-          <option>	San Bartolome	</option>
-          <option>	Saint Kitts and Nevis	</option>
-          <option>	Saint Eustace	</option>
-          <option>	Saint Martin	</option>
-          <option>	Saint Martin	</option>
-          <option>	Saint Pierre and Miquelon	</option>
-          <option>	St. Vincent and the Grenadines	</option>
-          <option>	St. Lucia	</option>
-          <option>	Surinam	</option>
-          <option>	Trinidad and Tobago	</option>
-          <option>	Uruguay	</option>
-          <option>	Venezuela	</option>               
-
-            </optgroup>
-            <optgroup label="Europe">
-              <option >Berlin</option>
-              <option>Paris</option>
-              <option>London</option>
-              <option>Madrid</option>
-              <option>Vienna</option>
-              <option>Brussels</option>
-              <option>Moscow</option>
-              <option>Sofia</option>
-              <option>Copenhagen</option>
-              <option>Athens</option>
-              <option>Budapest</option>
-              <option>Reykjavik</option>
-              <option>Dublin</option>
-              <option>Rome</option>
-              <option>Amsterdam</option>
-              <option>Oslo</option>
-              <option>Warsaw</option>
-              <option>Lisabon</option>
-              <option>Bern</option>
-              <option>Kiev</option>
-              <option>Stockholm</option>
-            </optgroup>
-            <optgroup label="Asia">
-              <option>Kabul</option>
-              <option>Dhaka</option>
-              <option>Peking</option>
-              <option>Tiflis</option>
-              <option>New Delhi</option>
-              <option>Jakarta</option>
-              <option>Teheran</option>
-              <option>Baghdad</option>
-              <option>Jerusalem</option>
-              <option>Tokyo</option>
-              <option>Kuala Lumpur</option>
-              <option>Ulan Bator</option>
-              <option>Kathmandu</option>
-              <option>Singapore</option>
-              <option>Seoul</option>
-              <option>Ankara</option>
-              <option>Abu Dhabi</option>
-            </optgroup>
-            <optgroup label="Africa">
-              <option>Algiers</option>
-              <option>Luanda</option>
-              <option>Cairo</option>
-              <option>Nairobi</option>
-              <option>Tripoli</option>
-              <option>Windhoek</option>
-              <option>Pretoria</option>
-            </optgroup>
-            <optgroup label="Oceanic">
-              <option>Canberra</option>
-              <option>New ealand</option>
-            </optgroup>
-          
+          {PAIS_O_CIUDAD[region].map( (pais_o_ciudad) => <option onClick={ () => {if(pais_o_ciudad === "Argentina") {setProvincia(1)} else {setProvincia(0)}}}>{pais_o_ciudad}</option>)} 
           
         </select>
-        <br />        
+        <br />
+
         <select className="form-select" required aria-label="select example" id="inputCity"
           {
             ...register('provincia',{
             required:'debe ingresar provincia',})
           }>
-                        <option>Beni</option>
-            <option>Canberra</option>
-          <option>Jujuy</option>
-          <option>Salta</option>
-          <option>Tucuman</option>
+            {PROVINCIAS[provincia].map( (provincia) => <option>{provincia}</option>)}
         </select>
         <br />
+
         <input className="form-control"   placeholder='Ingresar la Latitud'
           {
             ...register('latitud',{
@@ -211,6 +87,7 @@ const CardCreation = () => {
           }
         />
         <br/>
+
         <input className="form-control"
           placeholder='Ingresar la Longitud'
           {
@@ -219,12 +96,14 @@ const CardCreation = () => {
           }
         />
         <br/>
+
         </div>
           <div className="d-grid gap-2">
             <button className='btn-form btn btn-secondary'>
             Crear Tarjeta
             </button>
           </div>
+
       </form>
     </div>
     </div>
---
+++ src/routes/Card/list_of_database_time_zone.js
@@ -0,0 +1,15 @@
+
+
+export const CONTINENTE = ["America", "Africa", "Asia", "Atlantic", "Europe"]
+
+const AMERICA = ["Anguilla","Antigua","Araguaina","Argentina","Aruba","Asuncion","Atikokan","Bogota","Cancun","Caracas","Chicago","Fortaleza","Guadeloupe","Guayaquil","La_Paz","Lima","Managua","Merida","Mexico_City","Monterrey","Montevideo","New_York","Phoenix","Santiago","Tijuana","Toronto","Vancouver","Virgin","Whitehorse"]
+const AFRICA = ["Abidjan","Accra","Addis_Ababa","Algiers"]
+const ASIA = ["Aden","Almaty","Amman","Anadyr","Aqtau","Aqtobe","Ashgabat","Ashkhabad"]
+const ATLANTIC = ["Azores","Bermuda","Canary","Cape_Verde","Faeroe","Faroe","Jan_Mayen","Madeira","Reykjavik"]
+const EUROPE = ["Amsterdam","Andorra","Athens","Belgrade","Berlin","Bratislava","Brussels","Budapest","Copenhagen","Dublin","Gibraltar","Kiev","Lisbon","London","Madrid","Monaco","Paris","Rome","Zurich"]
+
+export const PAIS_O_CIUDAD = [AMERICA, AFRICA, ASIA, ATLANTIC, EUROPE] 
+
+const ARGENTINA = ["Buenos_Aires","Catamarca","ComodRivadavia","Cordoba","Jujuy","La_Rioja","Mendoza","Rio_Gallegos","Salta","San_Juan","San_Luis","Tucuman","Ushuaia"]
+export const PROVINCIAS = [["No es necesario"],ARGENTINA]
+
